a8bce839f3b7955d264c3551413ef72d860d6c77
km-saifullah <kmsaifullah16@gmail.com>
2023-10-15 12:34:51 +0600

    initial setup done!

---
+++ js/tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      maxWidth: {
+        container: "1180px",
+      },
+      spacing: {
+        128: "32rem",
+      },
+      colors: {
+        navbarBG: "#EEEDDE",
+      },
+      fontFamily: {
+        sans: ["ui-sans-serif", "system-ui"],
+      },
+      fontSize: {
+        "main-heading": "64px",
+      },
+      lineHeight: {
+        "extra-loose": "2.5",
+        12: "3rem",
+      },
+      backgroundImage: {
+        "hero-pattern": "url()",
+      },
+    },
+  },
+};
