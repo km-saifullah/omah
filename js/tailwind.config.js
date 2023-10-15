71d1bc8f861d0cf221b8113aefb2c9a8a98a11a5
km-saifullah <kmsaifullah16@gmail.com>
2023-10-15 15:01:58 +0600

    navbar done![D[D[D[B[A[A[B[B navbae section done!

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -9,9 +9,11 @@ tailwind.config = {
       },
       colors: {
         navbarBG: "#EEEDDE",
+        "font-color": "#203239",
       },
       fontFamily: {
         sans: ["ui-sans-serif", "system-ui"],
+        poppins: "'Poppins', sans-serif",
       },
       fontSize: {
         "main-heading": "64px",
